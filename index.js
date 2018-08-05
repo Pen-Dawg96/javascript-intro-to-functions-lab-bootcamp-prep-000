5c5cf3db76bf62c4d34c7c6c79c865fa1baf6b0c
Penny Cornell <penny.cornell1@hotmail.com>
2018-08-05 17:54:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,17 @@ function logWhisper(string){
   console.log('hello');
 }
 function sayHiToGrandma(string){
+<<<<<<< HEAD
   if(string === string.toLowerCase()){
     return ("I can\'t hear you!")
   }
   else if(string === string.toUpperCase()){
+=======
+  if('hello'){
+    return ("I can\'t hear you!")
+  }
+  else if('HELLO'){
+>>>>>>> 843752752e9f362c012efff8c654a218257fcc2b
     return ('YES INDEED!')
   }
   else if("I love you, Grandma."){
